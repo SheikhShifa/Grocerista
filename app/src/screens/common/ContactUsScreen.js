28be93b81709e7dbf6c7ba88bfdcd9d037bd1379
SheikhShifa <shifa2000sheikh@gmail.com>
2024-08-19 11:48:41 +0530

    Project Uploaded

---
+++ app/src/screens/common/ContactUsScreen.js
@@ -0,0 +1,16 @@
+import Navigation from "../../components/Navigation";
+import Footer from "../../components/Footer"; 
+
+const ContactUsScreen = () => {
+    return (
+        <div>
+            <Navigation/>
+            <div className="main btn4">
+                <h3>Contact Us : </h3>
+                <h5>Email : GROCERISTA@gmail.com</h5>
+                <h5>Contact No : +91 8788774689</h5>
+            </div> 
+        </div>    
+    );
+}
+export default ContactUsScreen
